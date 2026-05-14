1b83bd166620e0fe3afe092e433f13987166879c
Felipe Cortez <fecortez.marcolino@gmail.com>
2026-05-14 13:05:38 -0300

    chore: arruma pastas

--- bin/deseq/scripts/quarto_wrapper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec /home/m40ista/miniconda3/bin/conda run -n deseq2_env --no-capture-output quarto "$@"
--- bin/deseq/scripts/radian_wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export R_HOME="/home/m40ista/miniconda3/envs/deseq2_env/lib/R"
-exec /home/m40ista/miniconda3/envs/deseq2_env/bin/radian "$@"
