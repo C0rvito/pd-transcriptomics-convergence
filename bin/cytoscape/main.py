545be413936bbb54f02f04050efc12230f8a3227
Felipe Cortez <fecortez.marcolino@gmail.com>
2026-05-22 22:06:30 -0300

    refactor: Adiciona módulo de processamento dos dados e 6 planilhas em data/processed:

--- bin/cytoscape/main.py
+++ bin/cytoscape/main.py
@@ -5,17 +5,13 @@ import py4cytoscape as p4c
 p4c.cytoscape_ping()       # deve retornar 'You are connected to Cytoscape!'
 p4c.cytoscape_version_info()
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
-from rich.console import Console
+from rich.console import Console, ConsoleOptions, RenderResult
+from rich.table import Table
 from rich.panel import Panel
 
 
 
 def main():
-    console = Console()
-    nome_programa = "Cytoscape"
-    descricao = f"{p4c.cytoscape_ping()}\n{p4c.cytoscape_version_info()}"
-    panel = Panel(f"[blue]{descricao}[/blue]", title=nome_programa)
-    console.print(panel)
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
--- bin/cytoscape/preprocessing/Processa.py
+++ bin/cytoscape/preprocessing/Processa.py
@@ -1,17 +1,75 @@
-import pandas as pd 
-import polars as pl  
-import time
+from typing import List, Dict
+from pathlib import Path
+import polars as pl
+import pandas as pd
+import gc
 
-# Pandas
-start = time.time()
-df_pandas = pd.read_csv('dados_gerados.csv')
-pandas_time = time.time() - start
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+_ESTE_ARQUIVO = Path(__file__).resolve()
+RAIZ          = _ESTE_ARQUIVO.parent.parent.parent.parent
 
-# Polars
-start = time.time()
-df_polars = pl.read_csv('dados_gerados.csv')
-polars_time = time.time() - start
+DADOS = RAIZ / "results"
+OUTPUTS = RAIZ / "data" / "processed" / "redes"
 
-print(f"Pandas read time: {pandas_time:.2f} seconds")
-print(f"Polars read time: {polars_time:.2f} seconds")
-print(f"Polars is {pandas_time/polars_time:.1f}x faster")
+LRRK2 = DADOS / "LRRK2_Ranked.xlsx"
+SNCA = DADOS / "SNCA_Ranked.xlsx"
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+def coleta_planilhas(file_path: Path, planilhas: List) -> None:
+    dfs_dict = pl.read_excel(
+        file_path,
+        sheet_name=planilhas,
+        engine="calamine"
+        )
+    for nome, df in dfs_dict.items():
+       arquivo_saida = f"{nome}.csv"
+       df.write_csv(OUTPUTS / arquivo_saida)
+       
+       print(f"Planilha '{nome}' processada e salva em: {OUTPUTS / arquivo_saida}")
+        
+    return 
+
+def concatena_up_down(arquivo_up: Path, arquivo_down: Path) -> pd.DataFrame:
+    GENE = arquivo_up.stem.split("_")[0]  # Exemplo: "LRRK2" ou "SNCA"
+    print(f"Processando {GENE}...")
+    
+    lf_up = pl.scan_csv(arquivo_up)
+    lf_down = pl.scan_csv(arquivo_down)
+
+    print("Concatenando os DataFrames...")
+    q = (
+        pl.concat([lf_up, lf_down])
+        .with_columns(
+            pl.when(pl.col("log2FoldChange") > 0)
+            .then(pl.lit("Up"))
+            .otherwise(pl.lit("Down"))
+            .alias("UP/DOWN")
+        )
+    )
+
+    q.collect().write_csv(OUTPUTS / f"{GENE}_Up_Down.csv")
+        
+    return q.collect().to_pandas()
+
+LRRK2_UP = OUTPUTS / "LRRK2_Isogenic_Up.csv"
+LRRK2_DOWN = OUTPUTS / "LRRK2_Isogenic_Down.csv"
+
+SNCA_UP = OUTPUTS / "SNCA_Up.csv"
+SNCA_DOWN = OUTPUTS / "SNCA_Down.csv"
+
+def main():
+    planilhas = ["LRRK2_Isogenic_Up", "LRRK2_Isogenic_Down", "SNCA_Up", "SNCA_Down"]
+    coleta_planilhas(LRRK2, planilhas[:2])
+    coleta_planilhas(SNCA, planilhas[2:])
+    lrrk2_df = concatena_up_down(LRRK2_UP, LRRK2_DOWN)
+    snca_df = concatena_up_down(SNCA_UP, SNCA_DOWN)
+
+    print(f"LRRK2 DataFrame:\n{type(lrrk2_df)}\n{lrrk2_df.head()}")
+    print(f"SNCA DataFrame:\n{type(snca_df)}\n{snca_df.head()}")
+
+    
+    return lrrk2_df, snca_df
+
+if __name__ == "__main__":
+    main()
+    gc.collect()
